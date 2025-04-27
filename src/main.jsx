018e2581da67ba1ee9f14a811b1e07a2f90be1e0
Arman <armangulistani@gmail.com>
2025-04-27 18:58:19 -0400

    Fix darktheme problem

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.jsx";
 import "./assets/global.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+// theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "light", // FORCE Light Mode
+  },
+});
+
+export default theme;
